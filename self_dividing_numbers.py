0a52f25a4eeeb906dcea8ea0db72157462b26604
Toren Darby <darby@science.ubc.ca>
2020-11-28 14:55:59 -0800

    Split out is_self_dividing function

--- self_dividing_numbers.py
+++ self_dividing_numbers.py
@@ -1,14 +1,13 @@
 import typing as t
 
 
-class Solution:
-    def selfDividingNumbers(self, left: int, right: int) -> t.List[int]:
-        return self_dividing_numbers(left, right)
-
-
 def self_dividing_numbers(left: int, right: int) -> t.List[int]:
-    digits = [int(digit) for digit in str(left)]
-    if 0 not in digits and all([left % digit == 0 for digit in digits]):
-        return [left]
-    else:
-        return []
+    return [num for num in range(left, right + 1) if _is_self_dividing(num)]
+
+
+def _is_self_dividing(num: int) -> bool:
+    digits = [int(digit) for digit in str(num)]
+
+    contains_0 = 0 in digits
+    self_dividing = not contains_0 and all([num % digit == 0 for digit in digits])
+    return self_dividing
--- test_self_dividing_numbers.py
+++ test_self_dividing_numbers.py
@@ -24,7 +24,20 @@ def test_when_left_right_11__return_1_1():
     expected = [11]
     assert expected == actual
 
-# def test_when_left_right_13__return_empty():
-#     actual = self_dividing_numbers.self_dividing_numbers(13, 13)
-#     expected = []
-#     assert expected == actual
+
+def test_when_left_right_13__return_empty():
+    actual = self_dividing_numbers.self_dividing_numbers(13, 13)
+    expected = []
+    assert expected == actual
+
+
+def test_when_1_9__return_1_through_9():
+    actual = self_dividing_numbers.self_dividing_numbers(1, 9)
+    expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert expected == actual
+
+
+def test_when_0_22():
+    actual = self_dividing_numbers.self_dividing_numbers(0, 22)
+    expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+    assert expected == actual
